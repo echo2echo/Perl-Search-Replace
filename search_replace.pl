d0c786ffac89e27605921af7faf3ff80c8d9f245
Joel Rader <echophotographic@gmail.com>
2021-05-13 20:37:34 -0400

    added search_replace.pl
    added script to github

---
+++ search_replace.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+# File name: search_replace.pl
+# Perl Script to Find and Replace Text Across Multiple Files and Directories
+# By Joel Rader  info[at]joelrader[dot]net
+
+# This script is very simple but is very powerful.  Be careful.
+# Backup files to be affected before running this script
+# This script is sensitive to special characters in the $replace and $replace_with variables
+
+use warnings;
+use strict;
+use diagnostics;
+
+my $file = "";
+my $count = 0;
+
+
+# find command below is now set to work with file "testMeFirst.html" for testing. Change filename to the file you are testing.
+# You may need to adjust permissions of this script, the "testMeFirst.html" file or your own file(s) for this script to run... 
+# The find command will get a list of all files ending in "html" if $command is set to find "*.html" excluding those without permission.
+# This script will skip updating files without permission ( grep -v "Permission denied" below )
+
+my $command = 'find . -name "testMeFirst.html" 2>&1 | grep -v "Permission denied"';
+my @output = `$command`;
+
+foreach(@output)
+{
+	my $lines = "";
+	$file = $_;
+
+	open(FILE, $file) or die "Can\'t open $_\n";
+	while (<FILE>)
+	{
+		$lines .= $_;
+	}
+	close(FILE);
+ 	
+	# Make changes here, the next two lines. Be careful with special characters.
+	my $replace = "mispeled word";
+        my $replace_with = "misspelled word";
+
+	if($lines =~ s/$replace/$replace_with/g)
+	{
+		print "$file";	
+		open(NEWFILE, ">$file") or die "Can\'t open $_\n";
+		print NEWFILE $lines;
+		close(NEWFILE);
+		$count += 1;
+	}
+}
+
+ print "$count file(s) updated.\n";
