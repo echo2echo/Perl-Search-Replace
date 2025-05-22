ef9fa05154adb1cbb240762a0be4ade7cac44f09
Joel Rader <echophotographic@gmail.com>
2025-05-22 12:10:00 -0400

    Update search_replace.pl
    comment wording

--- search_replace.pl
+++ search_replace.pl
@@ -16,7 +16,7 @@ my $count = 0;
 
 # find command below is now set to work with file "testMeFirst.html" for testing. Change filename to the file(s) you are testing.
 # You may need to adjust permissions of this script, the "testMeFirst.html" file or your own file(s) for this script to run... 
-# The find command will get a list of all files ending in "html" if $command is set to find "*.html" excluding those without permission. You could also set $command = 'find . -name  "*.*htm*"'; to include .htm, .shtml and .html files.
+# As an example the find command will get a list of all files ending in "html" if $command is set to find "*.html" excluding those without permission. You could also set $command = 'find . -name  "*.*htm*"'; to include .htm, .shtml and .html files.
 # This script will skip updating files without permission
 
 my $command = 'find . -name "testMeFirst.html"';
